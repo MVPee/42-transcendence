7e045eb5150c0472b50e080d778d8e97f1f46934
mvpee <mariusvanpee@hotmail.be>
2024-10-12 18:37:54 +0200

    better users implementation

--- backend/app/srcs/user/models.py
+++ backend/app/srcs/user/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,10 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     mail = models.EmailField()
     password = models.CharField(max_length=128, null=True)
+    elo = models.IntegerField(default=0)
+    win = models.IntegerField(default=0)
+    defeat = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} . . . . {self.mail} . . . . {self.created_at}"
