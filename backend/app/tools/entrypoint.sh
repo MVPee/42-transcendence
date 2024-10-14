5cae1e9bd3cec93f6f8274b946e24287323358cc
MVPee <mariusvanpee@hotmail.be>
2024-10-14 11:25:36 +0200

    add teamate user

--- backend/app/tools/entrypoint.sh
+++ backend/app/tools/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export DJANGO_SETTINGS_MODULE=app.settings
 
@@ -8,19 +8,32 @@ while ! nc -z ${POSTGRES_HOST} 5432; do
 done
 
 # Migrate server
-echo "/!\\   DEBUG: python manage.py migrate   /!\\"
+echo "/!\\   DEBUG: python manage.py migrate   /!\\" 
 python manage.py makemigrations
 python manage.py migrate
 
 # Create SUPERUSER
 echo "/!\\   DEBUG: Create SUPERUSER   /!\\" 
-cat << EOF > tools/create_superuser.py
+
+# Define superuser details in arrays
+USERNAMES=("$SUPERUSER_USERNAME" "$SUPERUSER1_USERNAME" "$SUPERUSER2_USERNAME" "$SUPERUSER3_USERNAME")
+EMAILS=("$SUPERUSER_EMAIL" "$SUPERUSER1_EMAIL" "$SUPERUSER2_EMAIL" "$SUPERUSER3_EMAIL")
+PASSWORDS=("$SUPERUSER_PASSWORD" "$SUPERUSER1_PASSWORD" "$SUPERUSER2_PASSWORD" "$SUPERUSER3_PASSWORD")
+
+for i in 0 1 2 3; do
+    username=${USERNAMES[$i]}
+    email=${EMAILS[$i]}
+    password=${PASSWORDS[$i]}
+
+    # Only proceed if the username is not empty
+    if [ -n "$username" ]; then
+        cat << EOF > tools/create_superuser.py
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = '${SUPERUSER_USERNAME}'
-email = '${SUPERUSER_EMAIL}'
-password = '${SUPERUSER_PASSWORD}'
+username = '${username}'
+email = '${email}'
+password = '${password}'
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
@@ -29,9 +42,15 @@ else:
     print('Superuser already exists.')
 EOF
 
-python manage.py shell < tools/create_superuser.py
+        python manage.py shell < tools/create_superuser.py
+    else
+        echo "Skipping SUPERUSER${i} as the username is not set."
+    fi
+done
+
+# Clean up the script
 rm -f tools/create_superuser.py
 
 # Launch server
-echo "/!\\   DEBUG: python manage.py runserver 0.0.0.0:8000   /!\\"
+echo "/!\\   DEBUG: python manage.py runserver 0.0.0.0:8000   /!\\" 
 exec "$@"
