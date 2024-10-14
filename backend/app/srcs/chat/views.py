c8cca07da63c08b43f0bf73e740ffeef3963bd6e
MVPee <mariusvanpee@hotmail.be>
2024-10-14 10:25:43 +0200

    add some ugly frontend but do the job now

--- backend/app/srcs/chat/views.py
+++ backend/app/srcs/chat/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
-def index(request):
+def index_view(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "You need to log in to access the profile page.")
+        return redirect('login')
     return render(request, 'chat/index.html')
