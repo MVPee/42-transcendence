70101e11818cbd5dc67990271329bb3de8484cfd
mvpee <mariusvanpee@hotmail.be>
2024-10-15 15:03:34 +0200

    add redis for instante data (but temporary)

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -97,6 +97,15 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [("redis", 6379)],
+        },
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
