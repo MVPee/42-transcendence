6ce8e8484d7676e7e39344e3797f99741816343b
mvpee <mariusvanpee@hotmail.be>
2024-10-18 12:08:26 +0200

    rm global chat and some frontend

--- backend/srcs/chat/urls.py
+++ backend/srcs/chat/urls.py
@@ -6,7 +6,6 @@ app_name='chat'
 
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("global/", views.global_chat_view, name="global_chat"),
     path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
     path('respond_friend_request/<int:friend_request_id>/', views.respond_friend_request, name='respond_friend_request'),
     path('unfriend/<int:friendship_id>/', views.unfriend, name='unfriend'),
--- backend/srcs/tournaments/views.py
+++ backend/srcs/tournaments/views.py
@@ -4,5 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    print(request.user.is_authenticated)
     return render(request, 'tournaments/index.html')
