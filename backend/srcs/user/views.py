3dda4dfa0bf6d37e16ee8608620de2e537820ddd
mvpee <mariusvanpee@hotmail.be>
2024-10-22 13:58:46 +0000

    add login and profile

--- backend/srcs/user/views.py
+++ backend/srcs/user/views.py
@@ -10,3 +10,23 @@ from .models import CustomUser as User
 
 def home_view(request):
     return render(request, 'base.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/#profile')
+        else:
+            messages.error(request, "Invalid username or password.")
+    return redirect('/#login')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('/#login')
