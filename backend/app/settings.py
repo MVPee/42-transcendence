880c17e1a6a00cae9a08c2328b539d00c4e11fc6
mvpee <mariusvanpee@hotmail.be>
2024-11-05 19:25:23 +0000

    only 42. for sub domain only

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -26,12 +26,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
-    'pong.mvpee.be',
     '42.mvpee.be',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://pong.mvpee.be',
     'https://42.mvpee.be',
 ]
 
