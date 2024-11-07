a93dfe8dacf3b3649403728aea9ff4c704724e14
mvpee <mariusvanpee@hotmail.be>
2024-11-07 13:30:26 +0000

    rm comment

--- backend/srcs/user/urls.py
+++ backend/srcs/user/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.base_view, name='base'),
-    re_path(r'^(?!static|admin|ssr|api).*/?$', views.base_view, name='base'),  # Redirige toutes les autres URL
+    re_path(r'^(?!static|admin|ssr|api).*/?$', views.base_view, name='base'),
 ]
