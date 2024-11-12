c6ab2304e3ed58f63dacae37f1d90fa951a3ae2e
mvpee <mariusvanpee@hotmail.be>
2024-11-12 14:34:27 +0000

    can't join the 2v2 match if it is finish

--- backend/srcs/ssr/views.py
+++ backend/srcs/ssr/views.py
@@ -200,7 +200,7 @@ class GameView(BaseSSRView):
 
         elif (matchMode == '2v2'):
             self.matchs = Matchs.objects.filter(Q(id=matchId) & Q(user1=request.user) | Q(user2=request.user) | Q(user3=request.user) | Q(user4=request.user)).first()
-            if self.matchs is not None:
+            if self.matchs is not None and self.matchs.team1_score < 5 or self.matchs.team2_score < 5:
                 self.page = 'play'
                 self.error_message = '2v2 is comming but not now bro.'
                 return super().get(request)
