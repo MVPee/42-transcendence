e88dca9d93e2ba57b7b2a8b33c81fe42cf09c790
mvpee <mariusvanpee@hotmail.be>
2024-11-19 09:28:19 +0000

    some fronty

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -1,8 +1,18 @@
 function displayStats(id) {
+    const allBtns = document.getElementsByClassName('btn-warning');
     const allStats = document.getElementsByClassName('Statistics');
     Array.from(allStats).forEach(element => {
         element.style.display = 'none';
     });
-    const choosedStat = document.getElementById(id);
-    choosedStat.style.display = 'block';
+    document.getElementById(`${id}-block`).style.display = 'block';
+
+    //Style
+    Array.from(allBtns).forEach(button => {
+        button.classList.remove('btn-warning');
+        button.classList.add('btn-outline-warning');
+    });
+
+    const selectedButton = document.getElementById(id);
+    selectedButton.classList.remove('btn-outline-warning');
+    selectedButton.classList.add('btn-warning');
 }
