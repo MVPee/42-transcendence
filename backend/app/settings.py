7de29dcf0488f3a55f600be72cdd865d94fcdb00
mvpee <mariusvanpee@hotmail.be>
2024-11-24 08:30:38 +0000

    add djangorestfull for the api request

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -36,6 +36,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
--- backend/srcs/api/views.py
+++ backend/srcs/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET'])
 def is_authenticated(request):
     """
     A api function who check if the user is authenticated or not.
@@ -9,4 +13,4 @@ def is_authenticated(request):
         JsonResponse: is authentificated (true/false)
     """
     is_authenticated = request.user.is_authenticated
-    return JsonResponse({'is_authenticated': is_authenticated})
+    return Response({'is_authenticated': is_authenticated}, status=status.HTTP_200_OK)
